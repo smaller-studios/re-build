1acc9d76715307316620fb5d36a3323ff2c98773
S-codes14 <smlmnguni14@gmail.com>
2021-08-01 04:04:26 +0200

    the react version of this app

---
+++ src/pages/react.js
@@ -0,0 +1,36 @@
+import React, { Fragment } from 'react'
+import Editor from 'blocks-ui'
+import * as Blocks from '@blocks/react'
+
+import SEO from '../components/seo'
+
+const JSX = `
+import React from 'react'
+import { Blocks, HeaderBasic } from '@blocks/react'
+
+export default () => (
+  <Blocks.Root>
+    <HeaderBasic>
+      <HeaderBasic.Logo to="/">Hello</HeaderBasic.Logo>
+      <HeaderBasic.Nav>
+        <HeaderBasic.Link to="/about">About</HeaderBasic.Link>
+        <HeaderBasic.Link to="/blog">Blog</HeaderBasic.Link>
+        <HeaderBasic.Link to="/contact">Contact</HeaderBasic.Link>
+      </HeaderBasic.Nav>
+    </HeaderBasic>
+  </Blocks.Root>
+)
+`
+
+const Layout = (props) => {
+  return <div className="layout">{props.children}</div>
+}
+
+const index = () => (
+  <Fragment>
+    <SEO title="React" />
+    <Editor src={JSX} blocks={Blocks} layout={Layout} />
+  </Fragment>
+)
+
+export default index
