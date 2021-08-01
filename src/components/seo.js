c64d132030a8a7fda28daddcea8ba5c4bb448132
S-codes14 <smlmnguni14@gmail.com>
2021-08-01 03:33:37 +0200

    seo - search engine optimiztion

---
+++ src/components/seo.js
@@ -0,0 +1,93 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+import { useStaticQuery, graphql } from 'gatsby'
+
+const SEO = ({ description, lang, meta, title }) => {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            author
+          }
+        }
+      }
+    `
+  )
+
+  const titleFormatted = title
+    ? `${title} — ${site.siteMetadata.title}`
+    : site.siteMetadata.title
+
+  const metaDescription = description || site.siteMetadata.description
+
+  return (
+    <Helmet
+      htmlAttributes={{
+        lang
+      }}
+      meta={[
+        {
+          name: `description`,
+          content: metaDescription
+        },
+        {
+          property: `og:title`,
+          content: titleFormatted
+        },
+        {
+          property: `og:description`,
+          content: metaDescription
+        },
+        {
+          property: `og:type`,
+          content: `website`
+        },
+        {
+          property: 'og:image',
+          content: site.siteMetadata.image
+        },
+        {
+          name: `twitter:card`,
+          content: `summary_large_image`
+        },
+        {
+          name: `twitter:creator`,
+          content: site.siteMetadata.author
+        },
+        {
+          name: `twitter:title`,
+          content: titleFormatted
+        },
+        {
+          name: `twitter:description`,
+          content: metaDescription
+        },
+        {
+          name: 'twitter:image',
+          content: site.siteMetadata.image
+        }
+      ].concat(meta)}
+    >
+      <title>{titleFormatted}</title>
+    </Helmet>
+  )
+}
+
+SEO.defaultProps = {
+  lang: `en`,
+  meta: [],
+  description: ``
+}
+
+SEO.propTypes = {
+  description: PropTypes.string,
+  lang: PropTypes.string,
+  meta: PropTypes.arrayOf(PropTypes.object),
+  title: PropTypes.string.isRequired
+}
+
+export default SEO
